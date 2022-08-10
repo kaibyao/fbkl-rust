d273fd6c7330f280c7dbe83da5e3644a9af853ba
Kai Yao <kai.b.yao@gmail.com>
2022-08-09 23:15:24 -0500

    basic actix server w/ tokio + eyre for error reporting is set up.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use color_eyre::{Result};
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    setup()?;
+
+    match HttpServer::new(|| {
+        println!("Starting fbkl/server on port 5432...");
+
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 5432))?
+    .run()
+    .await {
+        Ok(_) => Ok(()),
+        Err(e) => panic!("{}", e)
+    }
+}
+
+fn setup() -> Result<()> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() {
+        std::env::set_var("RUST_LIB_BACKTRACE", "1")
+    }
+    color_eyre::install()?;
+
+    Ok(())
 }
