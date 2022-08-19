6d8979ec3163ebfcb5fb445c220b9a3665db59d0
Kai Yao <kai.b.yao@gmail.com>
2022-08-19 00:31:56 -0500

    user::insert now takes a token argument

--- db/src/queries/user_queries.rs
+++ db/src/queries/user_queries.rs
@@ -5,20 +5,15 @@ use crate::{
 use diesel::{Insertable, RunQueryDsl, r2d2::{PooledConnection, ConnectionManager}, PgConnection, Connection};
 use diesel::result::Error;
 use diesel::insert_into;
-use rand::{rngs::OsRng, RngCore};
 use crate::models::user_token_model::TokenTypeEnum;
 
-pub async fn insert(user: InsertUser, conn: &mut PooledConnection<ConnectionManager<PgConnection>>) -> Result<(User, UserToken), Error> {
+pub async fn insert(user: InsertUser, token: Vec<u8>, conn: &mut PooledConnection<ConnectionManager<PgConnection>>) -> Result<(User, UserToken), Error> {
     conn.transaction::<(User, UserToken), _, _>(|conn| {
         let inserted_user: User = user.insert_into(users::table).get_result(conn)?;
 
-        // generate token
-        let mut token = [0u8; 16];
-        OsRng.fill_bytes(&mut token);
-
         let inserted_user_token: UserToken = insert_into(user_tokens::table).values(InsertUserToken {
             user_id: inserted_user.id,
-            token: token.into_iter().collect(),
+            token,
             token_type: TokenTypeEnum::RegistrationConfirm,
             sent_to: Some(inserted_user.email.clone()),
         }).get_result(conn)?;
